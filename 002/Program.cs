f9bb0d050c757b2c4ed55e024d2dc8e1917b4e0a
Natalia <zhuravlevanyu@mail.ru>
2023-01-20 23:15:46 +0300

    gitinore

---
+++ 002/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;//длина строки
+Console.Write(MakeArray(number, count));
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+/*выводим третью цифру числа. 
+Если 3 цифры нет, сообщает и выводит 0. Нагуглено*/
+int MakeArray(int a, int b)
+{
+    int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет.");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+    return result;
+}
